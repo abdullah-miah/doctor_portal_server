61499e6cf31e5dfe3c5ecbd2e5e4d8e0576273b1
abdullah-miah <mdabdullaha708@gmail.com>
2022-05-22 07:45:47 +0600

    all r commit here

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors')
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const app = express()
 const port =process.env.PORT|| 5000
@@ -17,11 +18,24 @@ async function run(){
     await client.connect();
     const serviceCollection = client.db('doctor_portals').collection('services')
     const bookingCollection = client.db('doctor_portals').collection('bookings')
+    const usersCollection = client.db('doctor_portals').collection('users')
   app.get('/service', async (req,res)=>{
       const query ={};
       const cursor = serviceCollection.find(query);
       const  services = await cursor.toArray();
       res.send(services);
+  })
+  app.put('/user/:email', async (req, res)=>{
+    const email = req.params.email;
+    const user = req.body;
+    const filter ={email: email};
+    const options = {upsert: true};
+    const updateDoc ={
+      $set: user
+    };
+    const result = await usersCollection.updateOne(filter, updateDoc, options);
+    const token =jwt.sign({email: email}, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
+    res.send({result, token});
   })
    // Warning: This is not the proper way to query multiple collection. 
     // After learning more about mongodb. use aggregate, lookup, pipeline, match, group
